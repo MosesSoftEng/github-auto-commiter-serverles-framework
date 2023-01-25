e743b34175274f831fbc71ab897dff5dc82cc2f1
Moses Mwangi <99524902+MosesSoftEng@users.noreply.github.com>
2023-01-25 20:40:26 +0300

    Add comments

--- src/functions/hello/index.ts
+++ src/functions/hello/index.ts
@@ -1,19 +1,26 @@
 import schema from './schema';
 import { handlerPath } from '@libs/handler-resolver';
 
+/*
+ * Define AWS Lambda function properties here.
+ */
 export default {
-  handler: `${handlerPath(__dirname)}/handler.main`,
-  events: [
-    {
-      http: {
-        method: 'post',
-        path: 'hello',
-        request: {
-          schemas: {
-            'application/json': schema,
-          },
+    // Hander property - Points to the file and module containing the code you want to run in your function.
+    handler: `${handlerPath(__dirname)}/handler.main`, // Reference to file handler.ts & exported function 'main'
+
+    // Event property - References Lambda events that trigger pointed AWS Lambda function.
+    events: [
+        {
+            // HTTP Event
+            http: {
+                method: 'post',
+                path: 'hello',
+                request: {
+                    schemas: {
+                        'application/json': schema,
+                    },
+                },
+            },
         },
-      },
-    },
-  ],
+    ],
 };
