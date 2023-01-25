adca97c4927db26cfcb7bd81ede3c84762848158
Moses Mwangi <99524902+MosesSoftEng@users.noreply.github.com>
2023-01-25 20:39:22 +0300

    Add function to save repo files content as a commit

--- src/logic/commits.ts
+++ src/logic/commits.ts
@@ -25,3 +25,44 @@ const getRepoFileDetails = async (commitDetails: CommitDetails) => {
 
     return file;
 };
+/**
+ * saveRepoFileContent
+ *
+ * Save details to a repository file.
+ *
+ * @param {object} commitDetails An object containing the owner, repo, and path of the repository.
+ * @param {object} committerDetails An object containing commiter information.
+ * @param {string} committerDetails A base64 string of file content to be saved.
+ * @param {string} sha Unique string for file to be updated.
+ * @param {string} message Commit message.
+ * 
+ * @returns {object} An object containing the file details.
+ */
+const saveRepoFileContent = async (
+    commitDetails: CommitDetails,
+    committerDetails: CommitterDetails,
+    content: string,
+    sha: string,
+    message: string
+) => {
+    let results = null;
+
+    try {
+        results = await octokit.request(`PUT /repos/${commitDetails.owner}/${commitDetails.repo}/contents/${commitDetails.path}`, {
+            ...commitDetails,
+            committer: {
+                name: committerDetails.name,
+                email: committerDetails.email,
+                date: committerDetails.date
+            },
+            content: content,
+            sha: sha,
+            message: message,
+        });
+    } catch (err: any) {
+        // Todo: Log error
+        results = err;
+    }
+
+    return results;
+};
