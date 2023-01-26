f962322627d524a23016729b652f2845e8034aef
Moses Mwangi <99524902+MosesSoftEng@users.noreply.github.com>
2023-01-26 14:07:43 +0300

    Create config.ts

---
+++ src/config/config.ts
@@ -0,0 +1,16 @@
+export default {
+    repoFile: {
+        repo: "githubemojis",
+        owner: "MosesSoftEng",
+        path: "README.md",
+    },
+
+    committer: {
+        name: "MosesSoftEng",
+        email: "moses.soft.eng@gmail.com",
+    },
+
+    schedule: {
+        interval: "12 hours"
+    }
+}
